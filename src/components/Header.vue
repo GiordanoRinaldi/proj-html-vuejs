bf14b31d1000edae4e5bc3d29d7a4c7dc266de0e
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-10-11 17:32:22 +0200

    aggiunto header (stato embrionale)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,50 @@
 <template>
     <div class="header">
+        <div class="logo">
+            <img src="../assets/img/logotype.png" alt="logo">
+        </div>
+        <div class="navbar">
+            <ul>
+                <li v-for="(name, index) in navbar" :key="index"><a :href="name.href">{{name.name}}</a></li>
+            </ul>
+        </div>
 
     </div>
 </template>
 
 <script>
 export default {
-    name: "header"
+    name: "header",
+    data(){
+        return{
+            navbar:[
+                {
+                    name: "HOME",
+                    href: "#"
+                },
+                {
+                    name: "ABOUT",
+                    href: "#"
+                },
+                {
+                    name: "PROJECT",
+                    href: "#"
+                },
+                {
+                    name: "SERVICES",
+                    href: "#"
+                },
+                {
+                    name: "BLOG",
+                    href: "#"
+                },
+                {
+                    name: "CONTACT",
+                    href: "#"
+                },
+            ]
+        }
+    }
 }
 </script>
 
