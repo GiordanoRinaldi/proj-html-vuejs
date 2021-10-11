63ee6147249e49f330bacda544541ddecef3a3a6
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-10-11 23:48:15 +0200

    secondo font + fontawesome + bottoni

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,12 @@
         <div class="container-fluid new-container">
           <div class="text-jumbo">
             <div> <span class="line"></span> ALWATS THE BEST WAY YOU NEED IT</div>
-            <h1>THE BEST BUSINESS CONSULTING</h1>
-            <button >READ MORE</button>
-            <button>></button>
+            <h1 class="secondFont">THE BEST BUSINESS CONSULTING</h1>
+            <div class="buttons d-flex">
+              <button class="Primarybutton me-4" >READ MORE</button>
+              <button class="playButton align-self-end"><i class="fas fa-caret-right"></i></button>
+            </div>
+            
           </div>
           
         </div>
@@ -32,6 +35,7 @@ export default {
 
 <style lang="scss">
   @import "./assets/style/common";
+  @import '~@fortawesome/fontawesome-free/css/all.css';
   #app {
     color: $mainTitlecolor;
     font-family: 'Montserrat', sans-serif;
@@ -41,11 +45,29 @@ export default {
     background-color: $principalBackground;
     background-image: url("./assets/img/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg");
     background-repeat: no-repeat;
-    background-position: top right;
+    background-position: right top;
 
     .text-jumbo {
       margin-top: 200px;
+      width: 600px;
     }
+
+    .secondFont{
+      font-size: 100px;
+    }
+  }
+
+  .playButton {
+    width: 55px;
+    height: 55px;
+    border-radius: 50%;
+    background-color: transparent;
+    border: 2px solid white;
+  }
+
+  .fas.fa-caret-right {
+    color: $buttonColor;
+    font-size: 30px;
   }
 
 
