eeff308f254775019812432838e8e7f81cea55f0
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-10-14 11:21:11 +0200

    finito footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        <div class="d-flex justify-content-between">
+        <div class="d-flex justify-content-between align-items-center">
             <div class="logo">
                 <img src="../assets\img\logotype-2-150x25.png" alt="">
             </div>
@@ -9,11 +9,20 @@
                     <li v-for="(name, index) in navbar" :key="index" class="d-inline-block ms-5"><a :href="name.href">{{name.name}}</a></li>
                 </ul>
             </div>
-            <div class="social">
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-linkedin-in"></i>
-                <i class="fab fa-facebook-f"></i>
-                <i class="fab fa-twitter"></i>
+            <div class="social d-flex align-items-center">
+                <div>
+                    <i class="fab fa-instagram me-3"></i>
+                </div>
+                <div>
+                    <i class="fab fa-linkedin-in me-3"></i>
+                </div>
+                <div>
+                    <i class="fab fa-facebook-f me-3"></i>
+                </div>
+                <div>
+                    <i class="fab fa-twitter me-3"></i>
+                </div>
+                
             </div>
         </div>
         <hr>
@@ -23,7 +32,7 @@
                 <br>
                 <a href="#">Privacy Environmental Policy</a>
             </div>
-            <div>
+            <div class="Copriryght">
                 Copriryght &copy; 2020 Phlox Consulting. All Rights Reserved.
             </div>
         </div>
@@ -71,8 +80,20 @@ export default {
 footer{
     height: 320px;
     background-color: $principalBackground;
-
-
+    padding: 80px 160px 0 160px;
+    hr{
+        margin-top: 60px;
+        margin-bottom: 40px;
+    }
+    ul{
+        margin-bottom: 0;
+    }
+    a{
+        color: $aColor;
+    }
+    .Copriryght{
+        color: $aColor;
+    }
 }
 
 </style>
