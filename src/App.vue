21384075223d92ae03489022b954dbba149e798a
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-10-14 12:12:34 +0200

    cambiata favicon

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@
 <script>
 import Header from "./components/Header.vue"
 import Main from "./components/Main.vue"
-import Footer from "./components/Footer.vue"
+import Footer from "./components/Footer.vue" 
 
 export default {
   name: 'App',
